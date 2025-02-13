44921fc21d6c788e7d6d5c25ed2288bd7a4d7e7a
dariopalmesi <dariopalmesi88@gmail.com>
2025-02-13 18:40:30 +0100

    add snack 2

--- main.js
+++ main.js
@@ -19,3 +19,24 @@ getPostTitle(2)
     .then(data => console.log(data.title))
     .catch(error => console.error(error))
 
+//  Snack 2
+// Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
+
+function lanciaDado() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Sto lanciando il dado');
+            const lancio = Math.floor(Math.random() * 6) + 1
+            const incastra = Math.random() < 0.2
+            if (incastra) {
+                reject('Il dado si è incastrato')
+            } else {
+                resolve(`Il numero uscito è: ${lancio}`)
+            }
+        }, 3000)
+    })
+}
+
+lanciaDado()
+    .then(messaggio => console.log(messaggio))
+    .catch(error => console.error(error))
